3722a4891a39fdac9b87490fb92d68dd365748e8
ola-kanakri <ola.kanakri12@gmail.com>
2022-07-23 23:13:10 +0300

    Done task 1

--- 22-July/task-1/src/App.js
+++ 22-July/task-1/src/App.js
@@ -14,14 +14,15 @@ function App() {
   return (
     <div className="App">
       <h1>Balance: {counter} </h1>
-      
+
       <label for="dep"> Deposit amount:  </label>
-      <input type="number" id="dep" name="dep"  value={ this.state.dep } onChange={ this.handleChange } />
-      <button onClick={() => dispatch(increment(5)) }> Deposit</button>
+      <input type="number" onChange={e => this.setState({ number: e.target.value })}/>
+
+      <button onClick={() => dispatch(increment(this.state.number)) }> Deposit</button>
 
       <br></br><br></br><br></br>
-      <button onClick={() => dispatch(decrement()) }> Withdrawl(50) </button>
-      <button onClick={() => dispatch(decrement(100)) }> Withdrawl(100) </button>
+      <button onClick={() => dispatch(decrement(50)) }> Withdrawl(50$) </button>
+      <button onClick={() => dispatch(decrement(100)) }> Withdrawl(100$) </button>
       {isLogged ?<h3>Valuable Information I shouldn't see</h3> : '' }
       
     </div>
